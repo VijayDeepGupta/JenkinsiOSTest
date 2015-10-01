e35c1cc5e3ae6b074516a787c78359955987fbc7
Vijay Deep Gupta <vijayg@kiwitech.com>
2015-10-01 19:07:23 +0530

    Initial Commit

---
+++ JenkinsiOSTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsiOSTest
+//
+//  Created by Vijay Deep Gupta on 10/1/15.
+//  Copyright (c) 2015 KiwiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JenkinsiOSTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JenkinsiOSTest
+//
+//  Created by Vijay Deep Gupta on 10/1/15.
+//  Copyright (c) 2015 KiwiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
